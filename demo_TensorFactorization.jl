0515b0d4763373f1962a16a30d92a3c2151703cc
Teppei Eda <teppei.e5349@gmail.com>
2019-02-09 13:10:45 +0900

    Create dummy data

--- demo_TensorFactorization.jl
+++ demo_TensorFactorization.jl
@@ -9,22 +9,57 @@ using Distributions
 # check the type and dimenstion of input, output for each function and write test case
 
 function test()
-    R = rand(2,2,10)
+    R = creat_dummy_data(2,2,4,0.5)
+    missing_mask = missing_mask(R, 0.7)
+    R_obs = deepcopy(R)
+    R_obs[missing_mask] = NaN
+
     N = size(R)[1]
     M = size(R)[2]
     K = size(R)[3]
 
     D = 2
+
+    # hyper parameter for U,V,T
     mu_0 = zeros(D)
     beta_0 = 1
     W_0 = Matrix{Float64}(I, D, D)  # parameter for Wishart dist, DxD matrix
     nu_0 = D  # parameter for Wishart dist
+
+    # hyper parameter for R
     rho_0 = ones(D)
+
+    # hyper parameter for α
+    mu~_0 = 1
+    W~_0::Matrix{Float64} = 0.04
 
     # initilize
     U,_,_ = init_model_params(W_0, nu_0, mu_0, beta_0, N)
     V,_,_ = init_model_params(W_0, nu_0, mu_0, beta_0, M)
     T = init_T(W_0, nu_0, rho_0, beta_0, K)
+end
+
+function creat_dummy_data(N::Int64, M::Int64, K::Int64, threshold::Float64)
+    R = rand(N,M,K)
+    mask = R .< threshold
+    R[mask] .= 0
+    R[.!mask] .= 1
+    return R
+end
+
+function test_create_dummy_data()
+    X = creat_dummy_data(2,2,4,0.5)
+    print(X)
+end
+
+function missing_mask(X::Array{Float64, 3}, missing_rate::Float64)
+    mask = rand(size(X)[1], size(X)[2], size(X)[3]) .< missing_rate
+    return mask
+end
+
+function test_missing_mask()
+    X = creat_dummy_data(2,2,4,0.5)
+    mask = missing_mask(X, 0.7)
 end
 
 """
@@ -99,5 +134,7 @@ function test_init_T()
     @assert (D, K) == size(matrix)
 end
 
-test_init_model_params()
-test_init_T()
+#test_init_model_params()
+#test_init_T()
+#test_create_dummy_data()
+test_missing_mask()
