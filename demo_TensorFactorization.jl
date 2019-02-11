08e74e772445ede5eebf6e8b8194715a4ce15b9e
Teppei Eda <teppei.e5349@gmail.com>
2019-02-11 15:47:15 +0900

    Sample Ui

--- demo_TensorFactorization.jl
+++ demo_TensorFactorization.jl
@@ -8,6 +8,8 @@ using PDMats
 #V: DxM
 #T: DxK
 
+#alpha: size(W_tilda)[1]xsize(W_tilda)[2]xL
+
 # check the type and dimenstion of input, output for each function and write test case
 
 function test()
@@ -58,13 +60,13 @@ function gibbs_sampling(R, mask, D, mu_0, beta_0, W_0, nu_0, rho_0, nu_tilde_0, 
 
     p = Progress(L)
     for l in 1 : L
-        alpha[:,:,l] = sample_alpha(nu_tilde_0, W_tilde_0, mask, R, U[:,:,l], V[:,:,l], T[:,:,l])[1][1]
+        alpha[:,:,l] = sample_alpha(nu_tilde_0, W_tilde_0, mask, R, U[:,:,l], V[:,:,l], T[:,:,l])
         mu_U, Lambda_U = sample_theta(U[:,:,l], beta_0, mu_0, W_0, nu_0)
         mu_V, Lambda_V = sample_theta(V[:,:,l], beta_0, mu_0, W_0, nu_0)
         mu_T, Lambda_T = sample_theta_T(T[:,:,l], beta_0, rho_0, W_0, nu_0)
 
         # Update U
-        Q::Matrix{Vector{Float64}} = zeros(M, K, D)
+        Q = zeros(M, K, D)
         for k in 1 : K
             for j in 1 : M
                 Q[j,k,:] = V[:,j,l] .* T[:,k,l]
@@ -75,7 +77,7 @@ function gibbs_sampling(R, mask, D, mu_0, beta_0, W_0, nu_0, rho_0, nu_tilde_0, 
         end
 
         # Update V
-        P::Matrix{Vector{Float64}} = zeros(N, K, D)
+        P = zeros(N, K, D)
         for k in 1 : K
             for i in 1 : N
                 P[i,k,:] = U[:,i,l] .* T[:,k,l]
@@ -86,7 +88,7 @@ function gibbs_sampling(R, mask, D, mu_0, beta_0, W_0, nu_0, rho_0, nu_tilde_0, 
         end
 
         # Update T
-        X::Matrix{Vector{Float64}} = zeros(N, M, D)
+        X = zeros(N, M, D)
         for i in 1 : N
             for j in 1 : M
                 X[i,j,:] = U[:,i,l] .* V[:,j,l]
@@ -127,7 +129,7 @@ function sample_TK(K, mu_, Lambda_, alpha, X, I, T_K_prev)
     acc_Lambda::Matrix{Float64} = zeros(size(Lambda_))
     for i in 1 : N
         for j in 1 : M
-            acc_Lambda += alpha * I[i,j,K] * (X[i,j] * X[i,j]')
+            acc_Lambda += alpha[1,1] * I[i,j,K] * (X[i,j,:] * X[i,j,:]')
         end
     end
     Lambda = Lambda_ + acc_Lambda
@@ -135,7 +137,7 @@ function sample_TK(K, mu_, Lambda_, alpha, X, I, T_K_prev)
     acc_mu = zeros(size(mu_))
     for i in 1 : N
         for j in 1 : M
-            acc_mu += alpha * I[i,j,K] * R[i,j,K] * X[i,j]
+            acc_mu += alpha[1,1] * I[i,j,K] * R[i,j,K] * X[i,j,:]
         end
     end
     mu = inv(Lambda) * (Lambda_ * T_K_prev + acc_mu)
@@ -147,7 +149,7 @@ function sample_Tk_mt2(k, mu_, Lambda_, alpha, X, I, T_prev, T_next)
     acc_Lambda::Matrix{Float64} = zeros(size(Lambda_))
     for i in 1 : N
         for j in 1 : M
-            acc_Lambda += alpha * I[i,j,k] * (X[i,j] * X[i,j]')
+            acc_Lambda += alpha[1,1] * I[i,j,k] * (X[i,j,:] * X[i,j,:]')
         end
     end
     Lambda = 2Lambda_ + acc_Lambda
@@ -155,7 +157,7 @@ function sample_Tk_mt2(k, mu_, Lambda_, alpha, X, I, T_prev, T_next)
     acc_mu = zeros(size(mu_))
     for i in 1 : N
         for j in 1 : M
-            acc_mu += alpha * I[i,j,k] * R[i,j,k] * X[i,j]
+            acc_mu += alpha[1,1] * I[i,j,k] * R[i,j,k] * X[i,j,:]
         end
     end
     mu = inv(Lambda) * (Lambda_ * (T_prev + T_next) + acc_mu)
@@ -167,7 +169,7 @@ function sample_T1(mu_, Lambda_, alpha, X, I, T_2)
     acc_Lambda::Matrix{Float64} = zeros(size(Lambda_))
     for i in 1 : N
         for j in 1 : M
-            acc_Lambda += alpha * I[i,j,1] * (X[i,j] * X[i,j]')
+            acc_Lambda += alpha[1,1] * I[i,j,1] * (X[i,j,:] * X[i,j,:]')
         end
     end
     Lambda = 2Lambda_ + acc_Lambda
@@ -185,22 +187,81 @@ Q_jk = V_j element_wise product T_k
 """
 function sample_Ui(i, mu_, Lambda_, alpha, R, Q, I)
     M, K, _ = size(Q)
-    acc_Lambda::Matrix{Float64} = zeros(size(Lambda_))
+    acc_Lambda = zeros(size(Lambda_))
     for k in 1 : K
         for j in 1 : M
-            acc_Lambda += alpha * I[i,j,k] * (Q[j, k] * Q[j, k]')
+            acc_Lambda += alpha[1,1] * I[i,j,k] * (Q[j,k,:] * Q[j,k,:]')
         end
     end
     Lambda = Lambda_ + acc_Lambda
 
-    acc_mu::Matrix{Float64} = zeros(size(mu_))
+    acc_mu = zeros(size(mu_))
     for k in 1 : K
         for j in 1 : M
-            acc_mu += alpha * I[i, j, k] * R[i, j, k] * Q[j, k]
+            acc_mu += alpha[1,1] * I[i, j, k] * R[i, j, k] * Q[j,k,:]
         end
     end
     mu = inv(Lambda)*(Lambda_ * mu_ + acc_mu)
     return rand(MvNormal(mu, PDMats.PDMat(Symmetric(inv(Lambda)))))
+end
+
+function test_sample_Ui()
+    R = creat_dummy_data(2,2,4,0.5)
+    mask = missing_mask(R, 0.7)
+    R_obs = deepcopy(R)
+    R_obs[mask] .= 0
+
+    # hyper parameter for dimension of U,V and T
+    D = 2
+
+    # hyper parameter for U,V,T prior
+    mu_0 = zeros(D)
+    beta_0 = 1
+    W_0 = Matrix{Float64}(I, D, D)  # parameter for Wishart dist, DxD matrix
+    nu_0 = D  # parameter for Wishart dist
+
+    # hyper parameter for R prior
+    rho_0 = ones(D)
+
+    # hyper parameter for alpha prior
+    nu_tilde_0 = 1
+    W_tilde_0::Matrix{Float64} = hcat([0.04])
+
+    N,M,K = size(R)
+
+    # initilize
+    U1,_,_ = init_model_params(W_0, nu_0, mu_0, beta_0, N)
+    V1,_,_ = init_model_params(W_0, nu_0, mu_0, beta_0, M)
+    T1 = init_T(W_0, nu_0, rho_0, beta_0, K)
+
+    L = 1
+    # Container
+    U = zeros(D, N, L+1)
+    V = zeros(D, M, L+1)
+    T = zeros(D, K, L+1)
+    alpha = zeros(size(W_tilde_0)[1],size(W_tilde_0)[2], L)
+
+    U[:,:,1] = U1
+    V[:,:,1] = V1
+    T[:,:,1] = T1
+
+    p = Progress(L)
+    for l in 1 : L
+        alpha[:,:,l] = sample_alpha(nu_tilde_0, W_tilde_0, mask, R, U[:,:,l], V[:,:,l], T[:,:,l])
+        mu_U, Lambda_U = sample_theta(U[:,:,l], beta_0, mu_0, W_0, nu_0)
+
+        # Update U
+        Q = zeros(M, K, D)
+        for k in 1 : K
+            for j in 1 : M
+                Q[j,k,:] = V[:,j,l] .* T[:,k,l]
+            end
+        end
+        for i in 1 : N
+            U[:,i,l+1] = sample_Ui(i, mu_U, Lambda_U, alpha[:,:,l], R_obs, Q, mask)
+        end
+    end
+    print(typeof(U), U)
 end
 
 function sample_theta_T(T, beta_, rho_, W_, nu_)
@@ -211,8 +272,6 @@ function sample_theta_T(T, beta_, rho_, W_, nu_)
     acc = zeros(size(W_))
     for k in 2 : K
         tmp = T[:, k] - T[:, k-1]
-        #print(tmp, typeof(tmp), typeof(acc), typeof(tmp * tmp'))
-        print(acc, tmp*tmp')
         acc += (tmp * tmp')
     end
     W = inv(inv(W_) + acc + (beta_/(1+beta)) * ((T[:,1] - rho_) * (T[:,1] - rho_)'))
@@ -254,7 +313,6 @@ function test_sample_theta_T()
     p = Progress(L)
     for l in 1 : L
         mu_T, Lambda_T = sample_theta_T(T[:,:,l], beta_0, rho_0, W_0, nu_0)
-        print(mu_T, Lambda_T)
     end
 end
 
@@ -343,11 +401,11 @@ function sample_alpha(nu_tilde, W_tilde, I, R, U, V, T)
     for k in 1 : K
         for i in 1 : N
             for j in 1 : M
-                acc[1,1] += I[i,j,k] * (R[i,j,k] - multi_dot(U[:,i],V[:,j],T[:,k]))
+                acc[1,1] += I[i,j,k] * (R[i,j,k] - multi_dot(U[:,i],V[:,j],T[:,k]))^2
             end
         end
     end
-    W::Matrix{Float64} = inv(inv(W_tilde) + acc)
+    W = inv(inv(W_tilde) + acc)
     return rand(Wishart(nu, PDMats.PDMat(Symmetric(W))))
 end
 
@@ -381,8 +439,7 @@ function test_sample_alpha()
     V1,_,_ = init_model_params(W_0, nu_0, mu_0, beta_0, M)
     T1 = init_T(W_0, nu_0, rho_0, beta_0, K)
 
-    sampled_alpha = sample_alpha(nu_tilde_0, W_tilde_0, mask, R_obs, U1, V1, T1)
-    print(typeof(sampled_alpha))
+   print(sample_alpha(nu_tilde_0, W_tilde_0, mask, R_obs, U1, V1, T1))
 end
 
 function creat_dummy_data(N::Int64, M::Int64, K::Int64, threshold::Float64)
@@ -487,4 +544,5 @@ end
 #test_multi_dot()
 #test_sample_alpha()
 #test_sample_theta() #-> [1.36413, 2.11034][2.20263 -1.40809; -1.40809 1.09076]
-test_sample_theta_T()
+#test_sample_theta_T() #-> [4.85556, 0.93177][0.374475 -0.760314; -0.760314 7.5789]
+test_sample_Ui()
